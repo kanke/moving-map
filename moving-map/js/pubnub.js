fced30a7355c52eba5d6b9c0013ae59cf390798c
Kanke <kanke.ishaku@netbuilder.com>
2016-12-03 21:33:11 +0000

    adding map

---
+++ moving-map/js/pubnub.js
@@ -0,0 +1,3 @@
+<script type="text/javascript" src="http://pubnub.github.io/eon/lib/eon-map.js"></script>
+<link type="text/css" rel="stylesheet" href="http://pubnub.github.io/eon/lib/eon.css" />
+<div id='map'></div>
